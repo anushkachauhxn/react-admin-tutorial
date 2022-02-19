6d82e943cabe4fcdca99c5bd38d002a272c7e07d
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-02-19 14:50:21 +0530

    Supporting Mobile Devices

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, Create, SimpleForm, ReferenceInput, SelectInput, TextInput } from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
+import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, Create, SimpleForm, ReferenceInput, SelectInput, TextInput, SimpleList } from 'react-admin';
 
 const postFilters = [
     <TextInput source="q" label="Search" alwaysOn />,
@@ -9,17 +10,28 @@ const postFilters = [
 ];
 
 function PostList(props) {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
     return (
         <List filters={postFilters} {...props}>
-            <Datagrid rowClick="edit">
-                <TextField source="id" />
-                <ReferenceField source="userId" reference="users">
-                    <TextField source="name" /> 
-                </ReferenceField>
-                {/* <ReferenceField> component fetches the reference data from `users`, and passes it as a record to its child component (<TextField>). */}
-                <TextField source="title" />
-                <EditButton />
-            </Datagrid>
+            {isSmall ? (
+                <SimpleList 
+                    primaryText={record => record.title} 
+                    secondaryText={record => `${record.views} views`} 
+                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()} 
+                />
+                // Note: Since JSONRestServer doesn’t provide views or published_at values for posts, we switched to a custom API for those screenshots in order to demonstrate how to use some of the SimpleList component props.
+            ) : (
+                <Datagrid rowClick="edit">
+                    <TextField source="id" />
+                    <ReferenceField source="userId" reference="users">
+                        <TextField source="name" /> 
+                    </ReferenceField>
+                    {/* <ReferenceField> component fetches the reference data from `users`, and passes it as a record to its child component (<TextField>). */}
+                    <TextField source="title" />
+                    <EditButton />
+                </Datagrid>
+            )}
+            
         </List>
     );
 }
