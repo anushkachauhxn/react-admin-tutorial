9275e827747feb622296becbb1c6ca95b1bc9431
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-02-19 14:33:30 +0530

    Using a Custom Home Page: Dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import Dashboard from "./components/Dashboard";
 import UserList from "./components/Users";
 import PostList, { PostEdit, PostCreate } from "./components/Posts";
 import UserIcon from '@material-ui/icons/Group';
@@ -11,7 +12,7 @@ import PostIcon from '@material-ui/icons/Book';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 function App() {
   return (
-    <Admin dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} /> 
       <Resource name="users" list={UserList} icon={UserIcon} /> 
       {/* <Resource> = Each resource maps a name to an endpoint in the API.
