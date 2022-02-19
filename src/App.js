cde1f54e782ab14e91cf66a4a2de9722024fce74
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-02-19 12:05:46 +0530

    Selecting Columns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import UserList from "./components/Users";
 
 // dataProvider prop - a function capable of fetching data from an API
 // Since there is no standard for data exchanges between computers, you will probably have to write a custom provider to connect react-admin to your own APIs
@@ -8,7 +9,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={ListGuesser} /> 
+      <Resource name="users" list={UserList} /> 
       {/* <Resource> = Each resource maps a name to an endpoint in the API.
           >> Above code informs react-admin to fetch the “users” records from the https://jsonplaceholder.typicode.com/users URL
           + it should use should use the <ListGuesser> component to display the list of posts. */}
---
+++ src/components/Users.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { List, Datagrid, TextField, EmailField } from 'react-admin';
+
+function UserList(props) {
+    return (
+        <List {...props}>
+            <Datagrid rowClick="edit">
+                <TextField source="id" />
+                <TextField source="name" />
+                <EmailField source="email" />
+                <TextField source="phone" />
+                <TextField source="website" />
+                <TextField source="company.name" />
+                {/* Each Field component maps a different field in the API response, specified by the source prop. */}
+            </Datagrid>
+        </List>
+    );
+}
+
+export default UserList;
+
+// The <ListGuesser> component is not meant to be used in production - it’s just a way to quickly bootstrap an admin. 
+// => You’ll have to replace the ListGuesser component in the users resource (in App.js) by a custom React component.
+// => This is our custom React component to display the list of users.
+// <ListGuesser> component created one column for every field in the response. Here, we can remove the fields we don't want.
