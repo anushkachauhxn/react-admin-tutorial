5daf10c842743a05e1c9ade0fdb6313ac97388f5
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-02-19 13:25:54 +0530

    Adding Creation and Editing Capabilities: Part 1 (Edit)

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, SimpleForm, ReferenceInput, SelectInput, TextInput } from 'react-admin';
 
 function PostList(props) {
     return (
         <List {...props}>
             <Datagrid rowClick="edit">
                 <TextField source="id" />
-
                 <ReferenceField source="userId" reference="users">
-                    <TextField source="name" />
+                    <TextField source="name" /> 
                 </ReferenceField>
                 {/* <ReferenceField> component fetches the reference data from `users`, and passes it as a record to its child component (<TextField>). */}
-                
                 <TextField source="title" />
+                <EditButton />
             </Datagrid>
         </List>
     );
 }
 
+function PostEdit(props) {
+    return (
+        <Edit {...props}>
+            <SimpleForm>
+                <TextInput disabled source="id" />
+                <ReferenceInput source="userId" reference="users">
+                    <SelectInput optionText="name" />
+                </ReferenceInput>
+                <TextInput source="title" />
+                <TextInput multiline source="body" />
+            </SimpleForm>
+        </Edit>
+    );
+}
+
 export default PostList;
+export { PostEdit };
